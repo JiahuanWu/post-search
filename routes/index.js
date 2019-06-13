90799b7dfa9a0889ce8d5a3bd2412ac9ecd6e803
harmonycloud <harmonycloud@harmonyclouddeMacBook-Pro.local>
2019-06-13 11:07:05 +0800

    combine multiple lines address

--- routes/index.js
+++ routes/index.js
@@ -3,41 +3,53 @@ var router = express.Router();
 var fs = require('fs');
 var nGram = require('n-gram');
 
+//combine multiple lines address
+const getResult = (row) => {
+  let newArr = [];
+  //loop all the records
+  for(let i=0;i<row.length-1;i++){
+    let lastAddress ="";
+    if(row[i][2] == row[i+1][2] && row[i][8] != row[i+1][8]){
+      //if two consecutive records has the same id but different address, then combine them and jump to the next record
+      lastAddress = row[i][8]+row[i+1][8];
+      i++;
+    }else{
+      lastAddress = row[i][8]
+    }
+    newArr.push([row[i][2], row[i][6], row[i][7], lastAddress])
+  }
+  return newArr;
+}
 /* GET home page. */
 router.get('/', function (req, res, next) {
   var AdmZip = require('adm-zip');
   var request = require('request');
-  request.get({url:'http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip', encoding:null}, (err, res, body)=>{
+  request.get({ url: 'http://www.post.japanpost.jp/zipcode/dl/kogaki/zip/ken_all.zip', encoding: null }, (err, res, body) => {
     let zip = new AdmZip(body);
     let zipEntries = zip.getEntries();
-    zipEntries.forEach(zipEntry=>{
-      if(zipEntry.entryName == "KEN_ALL.CSV"){
+    zipEntries.forEach(zipEntry => {
+      if (zipEntry.entryName == "KEN_ALL.CSV") {
         //decode japanese
         const iconv = require('iconv-lite')
         let buf = zip.readFile(zipEntry);
         let str = iconv.decode(buf, 'Shift-JIS');
         //
         const csv = require('csvtojson');
-        csv({noheader:true,output:'csv'})
-        .fromString(str)
-        .then(row=>{
-          let newArr = [];
-          row.forEach(item=>{
-            newArr.push([item[2],item[6],item[7],item[8]])
+        csv({ noheader: true, output: 'csv' })
+          .fromString(str)
+          .then(row => {
+            let result = getResult(row);
+            const { parse } = require('json2csv');
+            try {
+              const csv = parse(result)
+              fs.writeFile('public/files/index.txt', csv, err => {
+                if (err) throw err;
+                console.log('write file succeed')
+              })
+            } catch (err) {
+              console.error(err)
+            }
           })
-          const {parse} = require('json2csv');
-          try{
-            const csv = parse(newArr)
-            fs.writeFile('public/files/index.txt',csv, err=>{
-              if(err) throw err;
-              console.log('write file succeed')
-            })
-          }catch(err){
-            console.error(err)
-          }
-          
-        })
-        
       }
     })
   })
@@ -45,9 +57,13 @@ router.get('/', function (req, res, next) {
 });
 //search
 router.get('/search', function (req, res, next) {
-  let result = [];
+  let result = [], words = [];
   //split input words
-  const words = nGram(2)(req.query.keyword);
+  if (req.query.keyword.length <= 2) {
+    words = nGram(1)(req.query.keyword)
+  } else {
+    words = nGram(2)(req.query.keyword);
+  }
   const filePath = 'public/files/index.txt'
   const stream = fs.createReadStream(filePath);
   let data, postArr;
@@ -60,10 +76,11 @@ router.get('/search', function (req, res, next) {
   stream.on('end', () => {
     postArr = data.split(/\n/)
     postArr.forEach(item => {
-      let flag = false;
-      words.forEach(word=>{
-        if(item.indexOf(word) >=0){
-          flag = true;
+      let flag = true;
+      words.forEach(word => {
+        //check if each word is in the string
+        if (item.indexOf(word) < 0) {
+          flag = false;
         }
       })
       if (flag) {
